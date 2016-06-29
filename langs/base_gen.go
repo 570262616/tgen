85748af942c452c0bcc1fbb0d1fc0dd1721fc4f2
hongjianzhu <zhu.life@gmail.com>
2016-06-29 15:06:52 +0800

    check duplicated names

--- langs/base_gen.go
+++ langs/base_gen.go
@@ -15,12 +15,27 @@ func (g *BaseGen) Init(lang string, parsedThrift map[string]*parser.Thrift) {
 	g.Lang = lang
 	g.Thrifts = parsedThrift
 	g.CheckNamespace()
+	g.CheckSameName()
 }
 
 func (g *BaseGen) CheckNamespace() {
 	for f, t := range g.Thrifts {
 		if _, ok := t.Namespaces[g.Lang]; !ok {
 			log.Fatalf("Namespace not found for language '%s' in file '%s'", g.Lang, f)
+		}
+	}
+}
+
+func (g *BaseGen) CheckSameName() {
+	for f, t := range g.Thrifts {
+		for _, s := range t.Services {
+			for _, st := range t.Structs {
+				if s.Name != st.Name {
+					continue
+				}
+
+				log.Fatalf("name of struct %q equals to service %q in file %q", st.Name, s.Name, f)
+			}
 		}
 	}
 }
